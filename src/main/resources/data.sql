0c6b7ea56c75f4aeadd7728e60bb87dadc4fc7f3
duransebas042-web <duransebas042@gmail.com>
2026-06-19 04:30:19 -0500

    Create data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+-- 🍛 INSERCIÓN AUTOMÁTICA DEL MENÚ DE LA SANJUANERA POS
+INSERT INTO platos (nombre, precio, disponible, categoria) VALUES 
+('Asado Huilense', 35000.00, true, 'Platos Fuertes'),
+('Hamburguesa Sanjuanera', 25000.00, true, 'Platos Fuertes'),
+('Tamal Huilense', 15000.00, true, 'Entradas'),
+('Patacones con Hogao', 12000.00, true, 'Entradas'),
+('Chicha Fría', 5000.00, true, 'Bebidas'),
+('Jugo de Maracuyá', 6000.00, true, 'Bebidas');
